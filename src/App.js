7abf7554e19413c1d36a07a9f546f595e1ba38a6
Jonathan Perapalanunt <jpapernotes@gmail.com>
2021-10-20 21:15:52 -0700

    Add basic list and map with ramen list

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Map, Marker } from "pigeon-maps"
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Map, Marker, ZoomControl } from "pigeon-maps";
 
+import yaml from 'js-yaml';
+import ramenYaml from './ramen.yml';
+
+/**
+ * 
+ * name
+ * rank
+ * review
+ */
 function App() {
-  const [lat, setLat] = useState(50.879);
-  const [lng, setLng] = useState(4.6997);
+  const [lat, setLat] = useState(37.3966639);
+  const [lng, setLng] = useState(-121.97524799999998);
+  const [reviews, setReviews] = useState([])
+  
+  useEffect(() => {
+    if (reviews.length === 0) {
+      fetch(ramenYaml)
+      .then(r => r.text())
+      .then(text => {
+        setReviews(yaml.load(text));
+      })
+    }
+  }, [reviews]);
+
+  function update(lat, lng) {
+    setLat(lat);
+    setLng(lng);
+  }
 
   return (
     <div className="App">
       <div>
-        <p>Hello</p>
+        <p>Header TODO</p>
       </div>
       <Container>
         <Row>
-          <Col sm={4} style={{backgroundColor: 'blue'}}>
-            List
+          <Col sm={4} style={{backgroundColor: 'beige'}}>
+            <ListGroup>
+              { reviews.map((review, index) => 
+                <ListGroup.Item onMouseEnter={(e) => {update(review.lat, review.lng)}} key={index}>{review.name}</ListGroup.Item>) 
+              }
+            </ListGroup>
           </Col>
           <Col sm={8} style={{height: '100vh'}}>
-            <Map height={500} defaultCenter={[lat, lng]} defaultZoom={11}>
+            <Map height={500} defaultCenter={[lat, lng]} center={[lat, lng]} defaultZoom={10}>
               <Marker width={50} anchor={[lat, lng]} />
+              <ZoomControl/>
             </Map>
           </Col>
         </Row>
