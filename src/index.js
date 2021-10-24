b9a48197700d833794ee969206c4b615f1fe29d9
Jonathan Perapalanunt <jpapernotes@gmail.com>
2021-10-24 14:41:10 -0700

    Use BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <HashRouter basename='/'>
+  <BrowserRouter basename='/'>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
